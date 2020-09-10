4341398416be96d73ccf32824301bd8c19644214
Bluemace <36447393+Bluemace@users.noreply.github.com>
2020-09-10 10:47:48 +0200

    test for pipeline

--- SimpleWebHalloWorld/Program.cs
+++ SimpleWebHalloWorld/Program.cs
@@ -5,7 +5,7 @@ namespace SimpleWebHalloWorld
 {
     public class Program
     {
-        //Test
+        //Test2
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
